2b63716bc6111ced832e03da44a4b604e7fcb4d9
Perfect Makanju <damiperfect@gmail.com>
2018-04-25 16:09:56 +0100

    Add favicon to website
    Also add dist folder to gitignore

--- static.config.js
+++ static.config.js
@@ -60,6 +60,7 @@ export default {
           <Head>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="shortcut icon" href="/favicon.png" />
             {renderMeta.styleTags}
           </Head>
           <Body>
