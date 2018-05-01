f101aa103225cd26f299743a16e766fd8475bba0
dreday <492591061@qq.com>
2018-05-01 21:34:28 +0800

    Add Modal once clicked to allow people to enter their names

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Col, Icon, Row } from 'antd';
+import { Button, Col, Form, Icon, Input, Modal, Row } from 'antd';
 
 import styled from 'styled-components';
 import MarketSubscriberForm from '@components/MarketSubscriberForm';
@@ -28,9 +28,25 @@ const PartnersEmail = {
 };
 
 class Cta extends React.Component {
+  state = {
+    subscriptionPopUpAlertVisiable: false,
+    subscriptionPopUpInputData: {},
+    subscriptionPopUpVisible: false
+  };
+
   render() {
+    const {
+      subscriptionPopUpVisible,
+      subscriptionPopUpInputData,
+      subscriptionPopUpAlertVisiable
+    } = this.state;
     return (
       <SectionWrapper id="subscribe">
+        <MarketSubscriberForm
+          alertVisiable={subscriptionPopUpAlertVisiable}
+          onCancel={() => this.setState({ subscriptionPopUpVisible: false })}
+          visible={subscriptionPopUpVisible}
+        />
         <Row type="flex" align="middle">
           <Col
             xs={24}
@@ -38,12 +54,22 @@ class Cta extends React.Component {
             md={12}
             lg={12}
             xl={12}
-            style={{ padding: '0 20px', marginTop: '70px' }}
+            style={{ padding: '0 20px', marginTop: '40px' }}
           >
-            <MarketSubscriberForm
-              title="Join our Newsletter"
-              hint="Enter your email here"
-            />
+            <MarketText style={{ fontSize: '24px', marginBottom: '30px' }}>
+              Join our Newsletter
+            </MarketText>
+            <Button
+              onClick={() => this.setState({ subscriptionPopUpVisible: true })}
+              type="primary"
+              style={{ width: '100%', textAlign: 'left' }}
+            >
+              Join Us{' '}
+              <Icon
+                type="arrow-right"
+                style={{ position: 'absolute', top: '35%', right: '15px' }}
+              />
+            </Button>
           </Col>
           <Col
             xs={24}
