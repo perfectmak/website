469e38d37efa5c233af2e90ce6a7982e0a76b706
brentoneill <brentoneill@gmail.com>
2018-05-01 21:06:22 -0600

    Issue #47 - Added new advisors to the /team page

--- src/assets/images/advisors/index.js
+++ src/assets/images/advisors/index.js
@@ -4,3 +4,5 @@ exports.CasperJohansen = require('./casper-johansen.svg');
 exports.DanHorowitz = require('./dan-horowitz.svg');
 exports.JoshFraser = require('./josh-fraser.svg');
 exports.PatrickCharles = require('./patrick-charles.svg');
+exports.RobBehnke = require('./rob-behnke.svg');
+exports.KevinOwocki = require('./kevin-owocki.svg');
