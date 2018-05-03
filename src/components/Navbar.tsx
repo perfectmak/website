5b8fd3723a1fbeaf41f78088bbcd61b9557a4e5f
brentoneill <brentoneill@gmail.com>
2018-05-03 08:40:33 -0600

    Updated link interface to Pascal case
    Fixed prefix of Link interface
    
    Removed withRouter
    
    Updated navbar after bad merge
    
    Reverted package.json to c1894ce

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
-import { Button, Layout, Menu, Popover } from 'antd';
+import { Button, Dropdown, Icon, Layout, Menu, Popover } from 'antd';
 import styled from 'styled-components';
 import { Link, scrollTo } from 'react-static';
 import { size } from '../breakpoints';
 import logoImg from '@images/logo_light.svg';
 
 const { Header } = Layout;
+const { SubMenu } = Menu;
 const externalLinks = ['/whitepaper', '/blog', '/faq', '/telegram'];
 
 const Logo = styled.div`
   line-height: 64px;
   margin: 0;
   float: left;
-
   @media (max-width: ${size.mobileS}) {
     max-width: 200px;
   }
 `;
 
 const StyledMenu = styled(Menu)`
+  .ant-dropdown-menu {
+    background: #fff;
+    border-radius: 25px;
+    padding: 10px;
+  }
+  .ant-dropdown-menu-item {
+    padding: 10px;
+    &:hover {
+      background-color: #fff;
+      color: #00e2c1;
+    }
+    &:first-child {
+      border-radius: 25px 25px 0 0;
+    }
+    &:last-child {
+      border-radius: 0 0 25px 25px;
+    }
+  }
   @media (max-width: ${size.tablet}) {
     &.ant-menu-dark {
       display: none;
     }
-
-    &.ant-menu-light.ant-menu-vertical {
+    &.ant-menu-light.ant-menu-inline {
       display: block;
       background: #fff;
       border-right: 0;
-
       .ant-menu-item-selected {
         border-radius: 12px;
         background-color: #f1f1f1;
@@ -48,18 +64,15 @@ const MobileMenuToggle = styled(Button)`
     height: 40px;
     line-height: 40px;
   }
-
   i {
     font-size: 20px;
     color: #fff;
   }
-
   @media (max-width: ${size.tablet}) {
     &.ant-btn {
       display: block;
     }
   }
-
   @media (max-width: ${size.mobileM}) {
     &.ant-btn {
       margin-left: 5px;
@@ -81,27 +94,88 @@ const HeaderWrapper = styled.div`
       padding-right: 30px;
     }
   }
-
   @media (max-width: ${size.mobileS}) {
     .ant-layout-header {
       padding-left: 20px;
       padding-right: 20px;
     }
   }
-
   .ant-popover-arrow {
     top: -4px;
   }
-
   .ant-popover-inner-content {
     padding-left: 10px;
     padding-right: 10px;
   }
-
   .ant-popover-inner {
     border-radius: 25px;
   }
 `;
+
+const DropdownTrigger = styled.a`
+  color: #fff;
+  float: left;
+  &:hover {
+    color: #00e2c1;
+  }
+  &:focus {
+    text-decoration: none;
+  }
+  @media (max-width: ${size.tablet}) {
+    color: #000;
+    display: none;
+    float: none;
+    padding: 0 16px;
+    line-height: 40px;
+    height: 40px;
+  }
+`;
+
+const StyledSubMenu = styled(SubMenu)`
+  display: none;
+  background: #fff;
+  .ant-menu {
+    background-color: #fff;
+    border-right: 0;
+  }
+  .ant-menu-item:hover,
+  .ant-menu-item-active,
+  .ant-menu:not(.ant-menu-inline) .ant-menu-submenu-open,
+  .ant-menu-submenu-active,
+  .ant-menu-submenu-title:hover {
+    border: 0;
+  }
+  @media (max-width: ${size.tablet}) {
+    display: block;
+  }
+`;
+
+const mobileWhitepaperLinks = styled(Menu.Item)`
+  display: none;
+  @media (max-width: ${size.tablet}) {
+    display: block;
+  }
+`;
+
+interface LinkInterface {
+  key: string;
+  label: string;
+  url: string;
+}
+
+const whitepapers: LinkInterface[] = [
+  {
+    key: '/whitepaper-english',
+    label: 'English',
+    url: 'https://www.marketprotocol.io/assets/MARKET_Protocol-Whitepaper.pdf'
+  },
+  {
+    key: '/whitepaper-chinese',
+    label: 'Chinese',
+    url:
+      'https://www.marketprotocol.io/assets/MARKET_Protocol-Whitepaper-Chinese.pdf'
+  }
+];
 
 class Navbar extends React.Component {
   state = {
@@ -116,7 +190,7 @@ class Navbar extends React.Component {
     }
   }
 
-  handleClick = e => {
+  handleClick = (e: React.ClickEvent) => {
     const path = e.key || e;
     if (!externalLinks.includes(path)) {
       this.setState({
@@ -127,16 +201,38 @@ class Navbar extends React.Component {
     if (path === location.pathname + location.hash) {
       const element = document.getElementById(resolvedHash);
       if (element !== null) {
-        scrollTo(element);
+        scrollTo(element as HTMLElement);
       }
     }
   }
 
+  renderWhitepaperDropdownMenu(whitepapersList: LinkInterface[]): JSX.Element {
+    return (
+      <Menu>
+        {whitepapersList.map(paper => {
+          return (
+            <Menu.Item key={paper.key}>
+              <Link
+                to={paper.url}
+                style={{ color: 'inherit', textDecoration: 'none' }}
+                target="_blank"
+              >
+                {paper.label}
+              </Link>
+            </Menu.Item>
+          );
+        })}
+      </Menu>
+    );
+  }
+
   renderMenuMarkup(breakpoint: string): JSX.Element {
+    const whitepaperLinks = this.renderWhitepaperDropdownMenu(whitepapers);
+
     return (
       <StyledMenu
         theme={breakpoint === 'desktop' ? 'dark' : 'light'}
-        mode={breakpoint === 'desktop' ? 'horizontal' : 'vertical'}
+        mode={breakpoint === 'desktop' ? 'horizontal' : 'inline'}
         style={{
           float: breakpoint === 'desktop' ? 'right' : 'none',
           lineHeight: '64px'
@@ -149,15 +245,19 @@ class Navbar extends React.Component {
             Team
           </Link>
         </Menu.Item>
-        <Menu.Item key="/whitepaper">
-          <Link
-            to="https://marketprotocol.io/assets/MARKET_Protocol-Whitepaper.pdf"
-            style={{ color: 'inherit', textDecoration: 'none' }}
-            target="_blank"
-          >
-            Whitepaper
-          </Link>
-        </Menu.Item>
+        <Dropdown
+          overlay={whitepaperLinks}
+          getPopupContainer={triggerNode =>
+            triggerNode.parentNode as HTMLElement
+          }
+        >
+          <DropdownTrigger className="ant-dropdown-link" href="#">
+            Whitepapers <Icon type="down" />
+          </DropdownTrigger>
+        </Dropdown>
+        <StyledSubMenu title={'Whitepapers'} trigger={'click'}>
+          {whitepaperLinks}
+        </StyledSubMenu>
         <Menu.Item key="/faq">
           <Link
             to="https://docs.marketprotocol.io/#faq-general"
