6af2dea217794b6fa05b3bc96eeca3f91b2b6eed
Phil Elsasser <phil@marketprotocol.io>
2018-06-06 13:44:18 -0600

    bash script fixed with travis env variables
    bash script no longer using travis_branch env variable for tags

--- scripts/clean_bucket.sh
+++ scripts/clean_bucket.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [[ -v TRAVIS_TAG ]]
-then
-  echo "Clearing Production Bucket Prior To Deployment (Leaving Discord and Telegram redirects)"
-  aws s3 rm s3://marketprotocol.io --recursive --exclude="telegram" --exclude="discord"
-elif [ "$TRAVIS_BRANCH" = "develop" ]
+echo "Attempting clean up of bucket for" $TRAVIS_BRANCH $TRAVIS_TAG
+
+if [ "$TRAVIS_BRANCH" = "develop" ]
 then
   echo "Clearing Dev Bucket Prior To Deployment (Leaving Discord and Telegram redirects)"
   eval export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
   eval export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
   aws s3 rm s3://dev.website.marketprotocol.io --recursive --exclude="telegram" --exclude="discord"
+elif [[ -v TRAVIS_TAG ]]
+then
+  echo "Clearing Production Bucket Prior To Deployment (Leaving Discord and Telegram redirects)"
+  aws s3 rm s3://marketprotocol.io --recursive --exclude="telegram" --exclude="discord"
 else
   echo "No deployment on this branch"
 fi
