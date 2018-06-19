09afc19b9d21445d7ef023725ab787433c8f4d26
Phil Elsasser <phil@marketprotocol.io>
2018-06-19 16:32:48 -0600

    fix issues with naming of jobs asset files

--- src/containers/Jobs/config.tsx
+++ src/containers/Jobs/config.tsx
@@ -1,14 +1,14 @@
 const list = [
   {
-    link: 'https://www.marketprotocol.io/assets/Head.ofEngineering.pdf',
+    link: 'https://www.marketprotocol.io/assets/Head-of-Engineering.pdf',
     title: 'Head of Engineering'
   },
   {
-    link: 'https://www.marketprotocol.io/assets/Senior.Front.End.Developer.pdf',
+    link: 'https://www.marketprotocol.io/assets/Senior-Front-End-Developer.pdf',
     title: 'Senior Front End Developer'
   },
   {
-    link: 'https://www.marketprotocol.io/assets/Senior Solidity Developer.pdf',
+    link: 'https://www.marketprotocol.io/assets/Senior-Solidity-Developer.pdf',
     title: 'Senior Solidity Developer'
   }
 ];
